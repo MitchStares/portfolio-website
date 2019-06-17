1b25e038c284ad110076a886a9b9612f52a96655
MitchStares <mitchell.stares@hotmail.com>
2019-06-17 22:32:22 +1000

    Login Page

---
+++ login_effect.js
@@ -0,0 +1,8 @@
+function pop() {
+  window.open(
+    "login.html",
+    "pop",
+    "height=400,width=400,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=auto,resizable=yes,copyhistory=no"
+  );
+}
+
---
+++ resources.md
@@ -0,0 +1,5 @@
+sites used for login stuff:
+http://talkerscode.com/webtricks/create-a-login-form-on-popup-box-using-jquery.php
+
+Useful colours:
+http://materialuicolors.co/?utm_source=launchers
